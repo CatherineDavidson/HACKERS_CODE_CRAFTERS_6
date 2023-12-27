a97a6a422ff748badc8d7189dd47e57213c53c6f
CatherineDavidson <144999318+CatherineDavidson@users.noreply.github.com>
2023-12-27 23:17:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    // For simplicity, just show the success message
+    document.getElementById('signup-form').style.display = 'none';
+    document.getElementById('success-message').style.display = 'block';
+}
